eb30f9f3aee7918fafd2c68954c0390d92b0c5df
pinaplehits <pinaplehits@outlook.com>
2022-03-01 07:14:44 -0700

    smdb to list to manipulate the data

--- read smdb.py
+++ read smdb.py
@@ -1,13 +1,25 @@
 import csv
-import glob
+
+fields = ['SHA256', 'Path', 'SHA1', 'MD5', 'CRC32', 'Size']
 
 path = r'\\RETROSMB\retronas\repos\MiSTer FPGA\Hardware-Target-Game-Database\EverDrive Pack SMDBs\Super EverDrive & SD2SNES SMDB.txt'
 
 with open(path, newline = '') as games:                                                                                          
-    game_reader = csv.reader(games, delimiter='\t')
-    data = list(game_reader)
+    game_reader = csv.reader(games, delimiter='\t') 
+    data = list(game_reader) 
+    
+    print(len(data))
 
-    """newlist = [x for x in glob.glob("*.txt")]
-    newlist.sort(key=str.lower)"""
-        
-    print(data[2])
+    print(data[0])
+    print(data[-1])
+
+    print(data[0][1])
+    print(len(data[0]))
+
+with open('Super EverDrive & SD2SNES SMDB.csv', 'w', newline = '') as f:
+      
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+      
+    write.writerow(fields)
+    write.writerows(data)
