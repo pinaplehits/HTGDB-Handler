2195c234cc96f0c4ac79cc695b94eb531d9d41c1
pinaplehits <pinaplehits@outlook.com>
2022-03-02 01:35:50 -0700

    all duplicates values are deleted

--- read smdb.py
+++ read smdb.py
@@ -17,25 +17,47 @@ with open(path, newline = '') as games:
     #print(data[0])
     #print(data[-1])
 
-    #print(data[6058][1])
+    #print(data[1][0])
 
     #Print the number of columns in the row
     #print(len(data[0]))
     
     #Print just the filename is splitted from the complete path
-    print(os.path.basename(data[6058][1]))
-
-    split_tup = os.path.splitext(os.path.basename(data[6058][1]))
+    """print(os.path.basename(data[6058][1]))
     
-    #Print file without extension
-    #Print file extension
-    print(split_tup[0])
-    print(split_tup[1])
+    split_text = os.path.splitext(os.path.basename(data[6058][1]))
+    file_name = split_text[0]
+    file_extension = split_text[1]
+    print(file_name)
+    print(file_extension)"""
+
+    """for i in data:
+        if len(i) != 6:
+            print(len(i))"""
+    
+    newdata = []
+    newdata.append(data[0])
+    x = 1
+    
+    for i in range(len(data)):
+        write = True
+        for j in range(len(newdata)):
+            if newdata[j][0] == data[i][0]:
+                write = False
+                break
+        if write:
+            x += 1
+            newdata.append(data[i])
+            print(newdata[-1])
+    
+    print(x)
+    print(len(data))
+    print(len(data) - x)
 
 with open('Super EverDrive & SD2SNES SMDB.csv', 'w', newline = '') as f:
       
     # using csv.writer method from CSV package
     write = csv.writer(f)
       
-    write.writerow(fields)
-    write.writerows(data)
+    #write.writerow(fields)
+    write.writerows(newdata)
