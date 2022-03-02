af0eb5cb8389b139ae25c0fede6ef562fcf08cd0
pinaplehits <pinaplehits@outlook.com>
2022-03-01 17:25:00 -0700

    add splitted filename from path, same with the file extension from file

--- read smdb.py
+++ read smdb.py
@@ -1,6 +1,8 @@
 import csv
+import os
 
 fields = ['SHA256', 'Path', 'SHA1', 'MD5', 'CRC32', 'Size']
+fileExtension = []
 
 path = r'\\RETROSMB\retronas\repos\MiSTer FPGA\Hardware-Target-Game-Database\EverDrive Pack SMDBs\Super EverDrive & SD2SNES SMDB.txt'
 
@@ -8,13 +10,27 @@ with open(path, newline = '') as games:
     game_reader = csv.reader(games, delimiter='\t') 
     data = list(game_reader) 
     
-    print(len(data))
+    #Print number of iterations in the SMDB file
+    #print(len(data))
 
-    print(data[0])
-    print(data[-1])
+    #Print first and last object in list
+    #print(data[0])
+    #print(data[-1])
 
-    print(data[0][1])
-    print(len(data[0]))
+    #print(data[6058][1])
+
+    #Print the number of columns in the row
+    #print(len(data[0]))
+    
+    #Print just the filename is splitted from the complete path
+    print(os.path.basename(data[6058][1]))
+
+    split_tup = os.path.splitext(os.path.basename(data[6058][1]))
+    
+    #Print file without extension
+    #Print file extension
+    print(split_tup[0])
+    print(split_tup[1])
 
 with open('Super EverDrive & SD2SNES SMDB.csv', 'w', newline = '') as f:
       
