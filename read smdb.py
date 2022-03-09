b460e42d00effb09ee6f807bf277251db70d7f3f
pinaplehits <pinaplehits@outlook.com>
2022-03-08 17:31:45 -0700

    Better structure for the code

--- read smdb.py
+++ read smdb.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 import csv
+from hashlib import new
 import os
 import shutil
 
@@ -14,58 +15,64 @@ fileExtension = []
 
 path = config['local']['directory']
 
-with open(path, newline = '') as games:                                                                                          
-    game_reader = csv.reader(games, delimiter='\t') 
-    data = list(game_reader) 
-    
-    #Print number of iterations in the SMDB file
-    #print(len(data))
+def populate_list():
+    with open(path, newline = '') as file:                                                                                          
+        csv_reader = csv.reader(file, delimiter='\t') 
+        data = list(csv_reader) 
+    return data
 
-    #Print first and last object in list
-    #print(data[0])
-    #print(data[-1])
+def single_file_db():
+    data = populate_list()
 
-    #print(data[1][0])
-
-    #Print the number of columns in the row
-    #print(len(data[0]))
-    
-    #Print just the filename is splitted from the complete path
-    #print(os.path.basename(data[6058][1]))
-    
-    """split_text = os.path.splitext(os.path.basename(data[6058][1]))
-    file_name = split_text[0]
-    file_extension = split_text[1]
-    print(file_name)
-    print(file_extension)"""
-
-    """for i in data:
-        if len(i) != 6:
-            print(len(i))"""
-    
     newdata = []
     newdata.append(data[0])
-    x = 1
-    
+
     for i in range(len(data)):
         write = True
         for j in range(len(newdata)):
             if newdata[j][0] == data[i][0]:
                 write = False
+                if len(newdata[j]) != len(data[i]):
+                    print(len(newdata[j]), len(data[i]))
                 break
         if write:
-            x += 1
             newdata.append(data[i])
-            print(newdata[-1])
-    
-    print(x)
-    print(len(data))
-    print(len(data) - x)
 
-with open('Super EverDrive & SD2SNES SMDB.txt', 'w', newline = '') as f:
-      
-    # using csv.writer method from CSV package
-    write = csv.writer(f, delimiter = '\t')
-      
-    #write.writerow(fields)
-    write.writerows(newdata)
+    print(f'SMDB reduced from {len(data)} to {len(newdata)} files')
+
+    return newdata
+
+def new_file(filename, extension, _delimiter):
+    with open(f'{filename}.{extension}', 'w', newline = '') as file:
+        csvwriter = csv.writer(file, delimiter = _delimiter)
+        #csvwriter.writerow(fields)
+        csvwriter.writerows(single_file_db())
+
+new_file('Super EverDrive & SD2SNES SMDB', 'txt', '\t')
+
+"""
+#Print number of iterations in the SMDB file
+#print(len(data))
+
+#Print first and last object in list
+#print(data[0])
+#print(data[-1])
+
+#print(data[1][0])
+
+#Print the number of columns in the row
+#print(len(data[0]))
+
+#Print just the filename is splitted from the complete path
+#print(os.path.basename(data[6058][1]))
+
+split_text = os.path.splitext(os.path.basename(data[6058][1]))
+file_name = split_text[0]
+file_extension = split_text[1]
+print(file_name)
+print(file_extension)
+
+for i in data:
+    if len(i) != 6:
+        print(len(i))
+    """
