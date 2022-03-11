09d3b98d2cea5d8951a97b3b318ba7e495935c09
pinaplehits <pinaplehits@outlook.com>
2022-03-11 00:56:37 -0700

    New entries for single files

--- read smdb.py
+++ read smdb.py
@@ -3,6 +3,7 @@ from hashlib import new
 import csv
 from json import load
 import os
+import glob
 
 config_file = 'config.ini'
 config = ConfigParser()
@@ -12,15 +13,15 @@ config.read(config_file)
 fields = ['SHA256', 'Path', 'SHA1', 'MD5', 'CRC32', 'Size']
 path = config['local']['directory']
 
-def load_config():
-        directory = os.getcwd()
-        print(directory)
-        directory = os.path.dirname(directory)
-        print(directory)
+def root_folder(_current_directory):
+        root = os.path.dirname(_current_directory)
+        root = root.replace(os.sep, '/')
+        print(root)
 
+        return root
 
-def populate_list():
-    with open(path, newline = '') as file:                                                                                          
+def populate_list(_path):
+    with open(_path, newline = '') as file:                                                                                          
         csv_reader = csv.reader(file, delimiter='\t') 
         csv_list = list(csv_reader) 
     return csv_list
@@ -44,8 +45,9 @@ def single_file_db(_data):
 
     return newdata
 
-def new_file(_filename, _extension, _delimiter, _data):
-    with open(f'{_filename}.{_extension}', 'w', newline = '') as file:
+def new_file(_path, _delimiter, _data):
+    with open(_path, 'w', newline = '') as file:
+    #with open('//RETROSMB/retronas/repos/HTGDB-Handler/No Duplicates SMDBs/new.txt', 'w', newline = '') as file:
         csvwriter = csv.writer(file, delimiter = _delimiter)
         #csvwriter.writerow(fields)
         csvwriter.writerows(_data)
@@ -65,15 +67,34 @@ def drop_first_folder(_path):
     _path = _path.split('/', 1)
     return _path[1]
 
-def main():
-    load_config()
-    data = populate_list()
+def get_smdb(root, origin_path):
+    dir_smdb = root + (config['local']['SMDBs'])
+    os.chdir(dir_smdb)
+    smdb_list = [x for x in glob.glob("*.txt")]
+    smdb_list.sort(key = str.lower)
+    
+    os.chdir(origin_path)
 
-    new_file('Super EverDrive & SD2SNES SMDB', 'txt', '\t', 
-            single_file_db(data))
-            
-    get_extensions(data)
-    print(drop_first_folder('SD2SNES/1 US - A-E/2020 Super Baseball (USA).sfc'))
-    data.clear()
+    return smdb_list
+
+def main():
+    current_directory = os.getcwd()
+    root = root_folder(current_directory)
+    smdb_list = get_smdb(root, current_directory)
+    
+    for i in range(len(smdb_list)):
+        #print(smdb_list[i])
+        path = root + (config['local']['single_SMDBs']) + smdb_list[i]
+        dir_smdb = root + (config['local']['SMDBs']) + smdb_list[i]
+        data = populate_list(dir_smdb)
+        get_extensions(data)
+        print(path)
+        new_file(path, '\t', 
+                single_file_db(data))
+        data.clear()
+
+    #get_extensions(data)
+    #print(drop_first_folder('SD2SNES/1 US - A-E/2020 Super Baseball (USA).sfc'))
+    
 
 main()
