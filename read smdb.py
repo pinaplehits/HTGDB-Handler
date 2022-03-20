85291236a3403cec38f7fe2bf49cf25496d893f2
pinaplehits <pinaplehits@outlook.com>
2022-03-20 04:00:03 -0600

    reducido el tiempo de busqueda para archivos equivalentes

--- read smdb.py
+++ read smdb.py
@@ -15,6 +15,13 @@ def populate_list(_path):
     return csv_list
 
 def single_file_db(_data):
+    dataset = set([item[0] for item in _data])
+    print(len(dataset))
+
+    if len(dataset) == len(_data):
+        print("SMDB not reduced")
+        return _data
+
     newdata = []
     newdata.append(_data[0])
 
@@ -28,7 +35,7 @@ def single_file_db(_data):
                 break
         if write:
             newdata.append(_data[i])
-
+  
     print(f'SMDB reduced from {len(_data)} to {len(newdata)} files')
 
     return newdata
@@ -79,55 +86,65 @@ def re_files(_data, _regularexpresion):
     return _data
 
 def remove_files(_data, _filename, _sha1, i):
-    filename = f'{_data[i][0]}_{_data[i][1]}.{_data[i][2]}'
+    filename = f'{_data[i][0]}_{_data[i][1]}.txt'
 
     for j in range(len(_data)):
-        if _data[j][0] == _filename and _data[j][1] != _sha1:
-            os.remove(filename) 
-            print(f'File {filename} removed')
-            return
+        if _data[j][0] == _filename:
+            if _data[j][1] != _sha1:
+                os.remove(filename) 
+                print(f'File {filename} removed')
+                return
+            if _data[j][1] == _sha1:
+                return
 
 def main():
     #Load config in file .ini
     orig_smdb, new_smdb = load_config()
 
+    #Set path for the workplace
     root = os.path.normpath(os.getcwd())
     repo_path = os.path.dirname(root)
     path_smdb = os.path.normpath(os.path.join(repo_path, orig_smdb))
 
+    #Get SMDB text files and the SHA-1 git head from the repo
     os.chdir(path_smdb)
     smdb_list = populate_files('*.txt')
     sha1 = git_head()
     
+    #Set the working directory to the current project
     os.chdir(root)
     os.chdir(os.path.basename(new_smdb))
     
-    path_empty = False
+    path_contains_files = False
 
     if len(os.listdir(os.getcwd())) != 0:
         actual_files = populate_files('*.txt')
-        actual_files = re_files(actual_files, '_|\.')
-        path_empty = True
+        actual_files = re_files(actual_files, '_|\.txt')
+
+        actual_sha1 = set([item[1] for item in actual_files])
+        print(actual_sha1)
+        path_contains_files = True
 
     for i in range(len(smdb_list)):
         split_text = os.path.splitext(smdb_list[i])
         filename = f'{split_text[0]}_{sha1}.txt'
 
-        if path_empty:
-            remove_files(actual_files, split_text[0], sha1, i)
-            
-            if os.path.exists(filename):
+        if os.path.exists(filename):
                 print(f'Filename {filename} already exists')
-                continue
-
-        print('Creating:', filename)   
+                continue    
+        
+        """if path_contains_files:
+            if len(actual_files) < i:
+                remove_files(actual_files, split_text[0], sha1, i)"""  
 
         smdb_location = os.path.join(path_smdb, smdb_list[i])
-
         data = populate_list(smdb_location)
+
+        print(len(data))
         #get_extensions(data)
 
         new_file(filename, '\t', 
                 single_file_db(data))
+        print(f'File {filename} is created',) 
 
 main()
