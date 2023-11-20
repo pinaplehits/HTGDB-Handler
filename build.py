11863ab278345c3cf980ac491511c97a54475b89
pinaplehits <pinaplehits@outlook.com>
2023-11-19 17:59:59 -0700

    Refactor build.py for improved performance

--- build.py
+++ build.py
@@ -1,27 +1,31 @@
-from compressHandler import uncompress
-from configparser import ConfigParser
-from datetime import datetime
-from gitHandler import git_file_status, git_commit, git_push
-from jsonHandler import get_top_level_keys, write_to_child
-from reducer import reducer
-from smdbHandler import get_smdb_with_missing, get_smdb_not_verified
 import csv
 import os
 import shutil
 import subprocess
+from configparser import ConfigParser
+from datetime import datetime
+from pathlib import Path
+
+from compressHandler import uncompress
+from gitHandler import git_file_status, git_commit, git_push
+from jsonHandler import get_top_level_keys, write_to_child
+from reducer import reducer
+from smdbHandler import get_smdb_with_missing, get_smdb_not_verified
 
 
-def load_config(_section="build_reduced", _file="config.ini"):
+def load_config(
+    section: str = "build_reduced", file: str = "config.ini"
+) -> tuple[dict, str]:
     config = ConfigParser()
-    config.read(_file)
+    config.read(file)
 
-    return dict(config.items(_section)), config["reducer"]["latest_reduced"]
+    return dict(config.items(section)), config["reducer"]["latest_reduced"]
 
 
-def select_database(_sha1):
+def select_database(sha1: str) -> tuple[list[str], list[str]]:
     json_keys = get_top_level_keys()
     missing = get_smdb_with_missing(json_keys)
-    not_verified = get_smdb_not_verified(_sha1, json_keys)
+    not_verified = get_smdb_not_verified(sha1, json_keys)
 
     basename = sorted(set(missing + not_verified))
 
@@ -66,70 +70,70 @@ def select_database(_sha1):
     return [], []
 
 
-def all_smdb(_json_keys):
+def all_smdb(json_keys: list[str]) -> tuple[list[str], list[str]]:
     os.system("clear" if os.name == "posix" else "cls")
 
-    smdb = [f"{key}.txt" for key in _json_keys]
+    smdb = [f"{key}.txt" for key in json_keys]
 
-    [print(index, value) for index, value in enumerate(_json_keys)]
+    [print(index, value) for index, value in enumerate(json_keys)]
     index = input("Select one SMDB file: ")
 
-    return [smdb[int(index)]], [_json_keys[int(index)]]
+    return [smdb[int(index)]], [json_keys[int(index)]]
 
 
-def update_missing(_missing, _basename):
-    if not os.path.exists(_missing):
-        write_to_child(_basename, "missing", [])
+def update_missing(missing: str, basename: str) -> None:
+    if not os.path.exists(missing):
+        write_to_child(basename, "missing", [])
         return
 
-    with open(_missing, "r") as f:
+    with open(missing, "r") as f:
         missing_data = list(csv.reader(f, delimiter="\t"))
-        write_to_child(_basename, "missing", missing_data)
+        write_to_child(basename, "missing", missing_data)
 
-    os.remove(_missing)
+    os.remove(missing)
 
 
-def build_from_masters(_masters, _basename, _romimport):
-    name = os.path.basename(_masters)
+def build_from_masters(masters: str, basename: str, romimport: str) -> bool:
+    name = os.path.basename(masters)
 
-    if not os.path.exists(_masters):
+    if not os.path.exists(masters):
         print(f"Masters folder {name} doesn't exist")
         return False
 
-    if not os.listdir(_masters):
+    if not os.listdir(masters):
         print(f"Masters folder {name} is empty")
         return False
 
-    _masters = os.path.join(
-        _masters,
-        f"{_basename}.7z.001"
-        if os.path.exists(os.path.join(_masters, f"{_basename}.7z.001"))
-        else f"{_basename}.7z",
+    masters = os.path.join(
+        masters,
+        f"{basename}.7z.001"
+        if os.path.exists(os.path.join(masters, f"{basename}.7z.001"))
+        else f"{basename}.7z",
     )
 
-    uncompress(_masters, _romimport)
+    uncompress(masters, romimport)
 
     return True
 
 
-def build_from_main(_folder, _romimport):
-    name = os.path.basename(_folder)
+def build_from_main(folder: str, romimport: str) -> bool:
+    name = os.path.basename(folder)
 
-    if not os.path.exists(_folder):
+    if not os.path.exists(folder):
         print(f"Main folder {name} doesn't exist")
         return False
 
-    if not os.listdir(_folder):
+    if not os.listdir(folder):
         print(f"Main folder {name} is empty")
         return False
 
-    if not os.path.exists(_romimport):
-        shutil.move(_folder, _romimport)
+    if not os.path.exists(romimport):
+        shutil.move(folder, romimport)
         return True
 
-    for root, _, files in os.walk(_folder):
-        relative_path = os.path.relpath(root, _folder)
-        romimport_dir = os.path.join(_romimport, relative_path)
+    for root, _, files in os.walk(folder):
+        relative_path = os.path.relpath(root, folder)
+        romimport_dir = os.path.join(romimport, relative_path)
 
         if not os.path.exists(romimport_dir):
             os.makedirs(romimport_dir)
@@ -140,40 +144,46 @@ def build_from_main(_folder, _romimport):
             if not os.path.exists(romimport_file):
                 shutil.move(folder_file, romimport_file)
 
-    shutil.rmtree(_folder)
+    shutil.rmtree(folder)
 
     return True
 
 
-def build_from_verify(_basename, _smdb, _mismatch):
+def build_from_verify(basename: str, smdb: str, mismatch: str) -> None:
     section, latest_reduced = load_config()
 
-    romimport = os.path.join(section["romimport"], _basename)
-    folder = os.path.join(section["folder"], _basename)
-    missing = _mismatch.replace("mismatch", "missing")
+    romimport = os.path.join(section["romimport"], basename)
+    folder = os.path.join(section["folder"], basename)
+    missing = mismatch.replace("mismatch", "missing")
 
     if not build_from_main(folder, romimport):
         return
 
     run_script(
-        section["script"], romimport, _smdb, folder, missing, _basename, latest_reduced
+        section["script"], romimport, smdb, folder, missing, basename, latest_reduced
     )
 
 
 def run_script(
-    _script, _romimport, _smdb, _folder, _missing, _basename, _latest_reduced
-):
+    script: str,
+    romimport: str,
+    smdb: str,
+    folder: str,
+    missing: str,
+    basename: str,
+    latest_reduced: str,
+) -> None:
     build = [
         "python",
-        _script,
+        script,
         "--input_folder",
-        _romimport,
+        romimport,
         "--database",
-        _smdb,
+        smdb,
         "--output_folder",
-        _folder,
+        folder,
         "--missing",
-        _missing,
+        missing,
         "--skip_existing",
         "--drop_initial_directory",
         "--file_strategy",
@@ -181,23 +191,23 @@ def run_script(
     ]
 
     try:
-        subprocess.run(build, check=True)
+        subprocess.check_call(build)
     except subprocess.CalledProcessError as e:
-        print(f"Error al ejecutar el comando: {e.returncode}")
+        print(f"Error executing the command: {e.returncode}")
         exit()
     except Exception as e:
-        print(f"Ocurrió un error: {e}")
+        print(f"An error occurred: {e}")
         exit()
 
-    shutil.rmtree(_romimport)
+    shutil.rmtree(romimport)
 
-    update_missing(_missing, _basename)
-    write_to_child(_basename, "verifiedWith", _latest_reduced)
-    write_to_child(_basename, "incorrectLocation", False)
+    update_missing(missing, basename)
+    write_to_child(basename, "verifiedWith", latest_reduced)
+    write_to_child(basename, "incorrectLocation", False)
 
     files = ["db.json"]
     date = datetime.now().strftime("%d/%m/%y %H:%M:%S")
-    git_message = f"Build updated {_basename} in db.json on {date}"
+    git_message = f"Build updated {basename} in db.json on {date}"
     changes = [item for item in files if git_file_status(item)]
 
     print(git_message)
@@ -205,31 +215,31 @@ def run_script(
     git_push()
 
 
-def build():
+def build() -> None:
     section, latest_reduced = load_config()
 
     smdb, basename = select_database(latest_reduced)
 
-    for smdb, basename in zip(smdb, basename):
-        print(f"Building {basename}")
-        missing = "missing_" + smdb
-        romimport = os.path.join(section["romimport"], basename)
+    for smdb_file, basename_file in zip(smdb, basename):
+        print(f"Building {basename_file}")
+        missing = "missing_" + smdb_file
+        romimport = os.path.join(section["romimport"], basename_file)
 
-        folder = os.path.join(section["folder"], basename)
-        smdb = os.path.join(section["smdb"], smdb)
-        masters = os.path.join(section["masters"], basename)
+        folder = os.path.join(section["folder"], basename_file)
+        smdb_file = os.path.join(section["smdb"], smdb_file)
+        masters = os.path.join(section["masters"], basename_file)
 
         if not build_from_main(folder, romimport):
-            if not build_from_masters(masters, basename, romimport):
+            if not build_from_masters(masters, basename_file, romimport):
                 continue
 
         run_script(
             section["script"],
             romimport,
-            smdb,
+            smdb_file,
             folder,
             missing,
-            basename,
+            basename_file,
             latest_reduced,
         )
 
