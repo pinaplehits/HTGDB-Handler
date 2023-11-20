497ff50ace4b41e57cec87bd16e0799612248bc3
pinaplehits <pinaplehits@outlook.com>
2023-11-20 15:19:43 -0700

    Refactor JSON handling functions

--- jsonHandler.py
+++ jsonHandler.py
@@ -2,7 +2,7 @@ import json
 import os
 
 
-def get_top_level_keys(json_file="db.json"):
+def get_top_level_keys(json_file: str = "db.json") -> list:
     try:
         with open(json_file) as f:
             return list(json.load(f).keys())
@@ -12,16 +12,16 @@ def get_top_level_keys(json_file="db.json"):
     return []
 
 
-def delete_key(_key, json_file="db.json"):
+def delete_key(key: str, json_file: str = "db.json") -> None:
     try:
         with open(json_file) as f:
             data = json.load(f)
 
         try:
-            data.pop(_key)
-            print(f"Deleted {_key}")
+            data.pop(key)
+            print(f"Deleted {key}")
         except KeyError:
-            print(f"Couldn't delete {_key}. Key not found.")
+            print(f"Couldn't delete {key}. Key not found.")
             return
 
         with open(json_file, "w") as f:
@@ -30,82 +30,87 @@ def delete_key(_key, json_file="db.json"):
         print(f"Error reading the JSON file: {e}")
 
 
-def write_to_child(_basename, _child, _data, _json_db="db.json"):
+def write_to_child(
+    basename: str, child: str, data: dict, json_db: str = "db.json"
+) -> None:
     try:
-        with open(_json_db, "r") as f:
+        with open(json_db, "r") as f:
             json_data = json.load(f)
     except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
         print(f"Error reading the JSON file: {e}")
         exit()
 
     try:
-        json_data[_basename][_child] = _data
+        json_data[basename][child] = data
     except KeyError as e:
         print(f"Error {e} not found in the JSON file")
         exit()
 
     try:
-        with open(_json_db, "w") as f:
+        with open(json_db, "w") as f:
             json.dump(json_data, f, indent=2, sort_keys=True)
     except IOError as e:
         print(f"Error writing to the JSON file: {e}")
         exit()
 
 
-def write_to_key(_key, _json_db="db.json"):
+def write_to_key(key: str, json_db: str = "db.json") -> None:
     try:
-        with open(_json_db, "r") as f:
+        with open(json_db, "r") as f:
             json_data = json.load(f)
     except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
         print(f"Error reading the JSON file: {e}")
         exit()
 
-    json_data.setdefault(_key, {})
+    json_data.setdefault(key, {})
 
     try:
-        with open(_json_db, "w") as f:
+        with open(json_db, "w") as f:
             json.dump(json_data, f, indent=2, sort_keys=True)
     except IOError as e:
         print(f"Error writing to the JSON file: {e}")
         exit()
 
 
-def read_from_child(_basename, _child, _json_db="db.json"):
+def read_from_child(basename: str, child: str, json_db: str = "db.json") -> dict:
     try:
-        with open(_json_db, "r") as f:
+        with open(json_db, "r") as f:
             json_data = json.load(f)
-            return json_data.get(_basename, {}).get(_child)
+            return json_data.get(basename, {}).get(child)
     except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
         print(f"Error reading the JSON file: {e}")
         return None
 
 
-def read_from_key(_key, _json_db="db.json"):
+def read_from_key(key: str, json_db: str = "db.json") -> dict:
     try:
-        with open(_json_db, "r") as f:
+        with open(json_db, "r") as f:
             json_data = json.load(f)
-            return json_data.get(_key)
+            return json_data.get(key)
     except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
         print(f"Error reading the JSON file: {e}")
         return None
 
 
-def create_child_json(_json_db, _child):
-    db = get_top_level_keys()
+def create_child_json(json_db: str, child: str) -> None:
+    if os.path.exists(json_db):
+        os.remove(json_db)
 
-    if os.path.exists(_json_db):
-        os.remove(_json_db)
+    try:
+        with open(json_db, "w") as f:
+            json.dump({}, f)
+    except IOError as e:
+        print(f"Error writing to the JSON file: {e}")
+        return
 
-    with open(_json_db, "w") as f:
-        json.dump({}, f)
+    print(f"Writing to {json_db}...")
 
-    print(f"Writing to {_json_db}...")
-    for key in db:
-        item = read_from_child(key, _child)
-        if item:
-            write_to_key(key, _json_db)
-            write_to_child(key, _child, item, _json_db)
-    print(f"Done writing to {_json_db}.")
+    for key in get_top_level_keys():
+        if item := read_from_child(key, child):
+            write_to_key(key, json_db)
+            write_to_child(key, child, item, json_db)
+
+    print(f"Done writing to {json_db}.")
 
 
 if __name__ == "__main__":
