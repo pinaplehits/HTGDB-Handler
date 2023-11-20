98fe334fb9c985283be703473dab09fb2fcf70fb
pinaplehits <pinaplehits@outlook.com>
2023-11-20 15:28:42 -0700

    Refactor smdbHandler.py

--- smdbHandler.py
+++ smdbHandler.py
@@ -1,67 +1,70 @@
-from jsonHandler import read_from_child
 import csv
 import os
+from jsonHandler import read_from_child
+from typing import List
 
 
-def get_smdb_with_missing(_json_keys, _json_file="db.json"):
-    return [key for key in _json_keys if read_from_child(key, "missing", _json_file)]
+def get_smdb_with_missing(
+    json_keys: List[str], json_file: str = "db.json"
+) -> List[str]:
+    return [key for key in json_keys if read_from_child(key, "missing", json_file)]
 
 
-def get_smdb_not_verified(_sha1, _json_keys, _json_file="db.json"):
+def get_smdb_not_verified(
+    sha1: str, json_keys: List[str], json_file: str = "db.json"
+) -> List[str]:
     return [
         key
-        for key in _json_keys
-        if (verified := read_from_child(key, "verifiedWith", _json_file)) != _sha1
+        for key in json_keys
+        if (verified := read_from_child(key, "verifiedWith", json_file)) != sha1
         or not verified
     ]
 
 
-def get_extensions(_items):
+def get_extensions(items: List[str]) -> List[str]:
     return sorted(
         {
             os.path.splitext(os.path.basename(item[1]))[1].lower()
-            for item in _items
+            for item in items
             if " " not in os.path.splitext(os.path.basename(item[1]))[1]
         }
     )
 
 
-def get_number_of_items(_path):
-    with open(_path) as file:
+def get_number_of_items(path: str) -> int:
+    with open(path) as file:
         return sum(1 for line in file)
 
 
-def get_files_with_extension(_path, _extension=".txt"):
+def get_files_with_extension(path: str, extension: str = ".txt") -> List[str]:
     return [
-        os.path.join(_path, file)
-        for file in os.listdir(_path)
-        if os.path.splitext(file)[1].lower() == _extension
+        os.path.join(path, file)
+        for file in os.listdir(path)
+        if os.path.splitext(file)[1].lower() == extension
     ]
 
 
-def get_all_smdb(_path, _extension=".txt"):
-    return sorted(
-        [file for file in os.listdir(_path) if file.endswith(_extension)], key=str.lower
-    )
+def get_all_smdb(path: str, extension: str = ".txt") -> List[str]:
+    return [file for file in os.listdir(path) if file.endswith(extension)]
 
 
-def read_file(_path, _delimiter="\t", _newline=""):
-    _path = os.path.normpath(_path)
-    with open(_path, newline=_newline) as file:
-        return list(csv.reader(file, delimiter=_delimiter))
+def read_file(path: str, delimiter: str = "\t", newline: str = "") -> List[List[str]]:
+    path = os.path.normpath(path)
+    with open(path, newline=newline) as file:
+        return list(csv.reader(file, delimiter=delimiter))
 
 
-def create_new_file(_path, _data, _delimiter="\t"):
-    print(f"Creating {os.path.basename(_path)} file...")
+def create_new_file(path: str, data: List[List[str]], delimiter: str = "\t") -> None:
+    print(f"Creating {os.path.basename(path)} file...")
 
-    with open(_path, "w", newline="") as f:
-        csv.writer(f, delimiter=_delimiter).writerows(_data)
+    with open(path, "w", newline="") as f:
+        csv.writer(f, delimiter=delimiter).writerows(data)
 
-    print(f"File {os.path.basename(_path)} is created")
+    print(f"File {os.path.basename(path)} is created")
 
 
-def reduced_master(_path="Reduced SMDBs/"):
-    data = combine_all_smdb(_path)
+def reduced_master(path: str = "Reduced SMDBs/") -> List[List[str]]:
+    data = combine_all_smdb(path)
     dataset = set([item[0] for item in data])
 
     return [
@@ -69,6 +72,6 @@ def reduced_master(_path="Reduced SMDBs/"):
     ]
 
 
-def combine_all_smdb(_path="Reduced SMDBs/"):
-    smdb = get_files_with_extension(_path)
+def combine_all_smdb(path: str = "Reduced SMDBs/") -> List[List[str]]:
+    smdb = get_files_with_extension(path)
     return [line for file in smdb for line in read_file(file)]
