20a0028132b9c282df492fcb3de80c66fadb7496
pinaplehits <pinaplehits@outlook.com>
2023-11-20 17:35:23 -0700

    Refactor virtualenv creation and activation functions

--- librariesHandler.py
+++ librariesHandler.py
@@ -4,20 +4,20 @@ import subprocess
 import sys
 
 
-def create_virtualenv(_path):
-    if os.path.isdir(_path):
+def create_virtualenv(path: str) -> None:
+    if os.path.isdir(path):
         return
 
-    builder = EnvBuilder(with_pip=True, prompt=_path)
-    builder.create(_path)
+    builder = EnvBuilder(with_pip=True, prompt=path)
+    builder.create(path)
 
 
-def activate_virtualenv(_path):
+def activate_virtualenv(path: str) -> None:
     activate_script = "Scripts/activate" if sys.platform == "win32" else "bin/activate"
-    activate_path = os.path.join(_path, activate_script)
+    activate_path = os.path.join(path, activate_script)
 
 
-def install_package():
+def install_package() -> None:
     # subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
     subprocess.check_call(
         [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"]
